906853f6fe233bb94e2ba6e15afca7a4b5584f4f
Arkadiy Drovosekov <arkdro@gmail.com>
2016-02-06 20:16:41 +0300

    add app

---
+++ src/shoot_app.erl
@@ -0,0 +1,16 @@
+-module(shoot_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    shoot_sup:start_link().
+
+stop(_State) ->
+    ok.
