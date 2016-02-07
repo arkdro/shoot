8dbb2b4799a35406732b31ee4b5b2b48398552df
Arkadiy Drovosekov <arkdro@gmail.com>
2016-02-07 14:40:53 +0300

    prepare state on init

--- src/shoot_field.erl
+++ src/shoot_field.erl
@@ -35,7 +35,9 @@ start_link() ->
 %%%===================================================================
 
 init([]) ->
-    {ok, #state{}}.
+    random:seed(now()),
+    State = init_state(),
+    {ok, State}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -56,4 +58,47 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+init_state() ->
+    Storage = prepare_and_fill_storage(Ngamers, Width, Height),
+    #state{
+       storage = Storage,
+       width = Width,
+       height = Height,
+       n_gamers = Ngamers
+      }.
+
+prepare_and_fill_storage(Ngamers, Width, Height) ->
+    Storage = prepare_storage(),
+    prepare_gamers(Storage, Ngamers, Width, Height).
+
+prepare_gamers(Storage, Ngamers, Width, Height) ->
+    F = fun(_, Acc) ->
+                {_, Acc2} = prepare_one_gamer(Acc, Width, Height),
+                Acc2
+        end,
+    lists:foldl(F, Storage, lists:seq(1, Ngamers)).
+
+prepare_one_gamer(Storage, Width, Height) ->
+    Args = [{x, X},
+            {y, Y}],
+    {ok, Pid} = shoot_gamer_sup:start_child(Args),
+    Storage2 = add_new_gamer(Storage, Pid, X, Y),
+    {Pid, Storage2}.
+
+add_new_gamer(Storage, Pid, X, Y) ->
+    Gamer = #gamer{
+               pid = Pid,
+               x = X,
+               y = Y
+              },
+    store_gamer(Storage, Gamer).
+
+
+prepare_storage() ->
+    ets:new(?SERVER, [named_table, {keypos, #gamer.pid}]).
+
+store_gamer(Storage, Gamer) ->
+    ets:insert(Storage, Gamer),
+    Storage.
 
