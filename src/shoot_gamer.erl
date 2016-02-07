301ee902c5594136898334f6f71ca9733dc8ff5d
Arkadiy Drovosekov <arkdro@gmail.com>
2016-02-07 17:02:11 +0300

    add move/1

--- src/shoot_gamer.erl
+++ src/shoot_gamer.erl
@@ -3,6 +3,7 @@
 -behaviour(gen_server).
 
 -export([
+         move/1,
          start_link/1
         ]).
 
@@ -23,6 +24,9 @@
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
 
+move(Player) ->
+    gen_server:call(Player, move).
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
@@ -34,6 +38,16 @@ init(Args) ->
                    limit = Limit},
     {ok, State}.
 
+handle_call(move, _From, #state{time=T1} = State) ->
+    case can_move(State) of
+        true ->
+            Reply = ok,
+            State2 = update_time(State),
+            {reply, Reply, State2};
+        false ->
+            Reply = {error, too_soon},
+            {reply, Reply, State}
+    end;
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
@@ -54,6 +68,14 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
+can_move(#state{time = T1, limit = Limit}) ->
+    T2 = os:timestamp(),
+    Delta = timer:now_diff(T2, T1),
+    Delta >= Limit.
+
+update_time(#state{time = T1} = State) ->
+    T = os:timestamp(),
+    State#state{time = T}.
 
 get_time_limit() ->
     shoot_util:get_env(time_limit, 100000).
